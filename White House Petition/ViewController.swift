980b7135f243c2b849a3565652409b706dfbcbdd
Aasem Hany <assemsoma541@gmail.com>
2022-11-20 08:33:12 +0200

    Initial Commit

---
+++ White House Petition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  White House Petition
+//
+//  Created by Aasem Hany on 20/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
