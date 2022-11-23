6a277bfa2a10c05adfa17c92b66055eeda233d3e
Aasem Hany <assemsoma541@gmail.com>
2022-11-23 02:47:09 +0200

    Create DetailVC, Render HTML Data inside Its Web View, Create a Duplicated VC For The Top Rated Petitions Tab Bar Tab & Show Error Message.

--- White House Petition/ViewController.swift
+++ White House Petition/ViewController.swift
@@ -13,15 +13,27 @@ class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Recent Petitions"
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        title = isTopRated() ? "Top Rated Petitions" : "Most Recent Petitions"
+        let urlString = isTopRated() ? "https://www.hackingwithswift.com/samples/petitions-2.json" : "https://www.hackingwithswift.com/samples/petitions-1.json"
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url){
                 // We're Okay To Parse That Data
                 parseJson(json: data)
                 tableView.reloadData()
+                return
             }
         }
+        showError()
+    }
+    
+    private func isTopRated() -> Bool {
+        return navigationController?.tabBarItem.tag == 1
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
     
     func parseJson(json:Data){
@@ -39,13 +51,19 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let currentCell = tableView.dequeueReusableCell(withIdentifier: "Cell",for: indexPath)
-//        var config = currentCell.defaultContentConfiguration()
-//        config.text = petitions?.results[indexPath.row].title ?? ""
-//        config.secondaryText = petitions?.results[indexPath.row].body ?? ""
-//        currentCell.contentConfiguration = config
+        //        var config = currentCell.defaultContentConfiguration()
+        //        config.text = petitions?.results[indexPath.row].title ?? ""
+        //        config.secondaryText = petitions?.results[indexPath.row].body ?? ""
+        //        currentCell.contentConfiguration = config
         currentCell.textLabel?.text = petitions?.results[indexPath.row].title ?? ""
         currentCell.detailTextLabel?.text = petitions?.results[indexPath.row].body ?? ""
         return currentCell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = PetitionDetailViewController()
+        vc.petition = petitions?.results[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
 
